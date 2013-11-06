232e1cf3a622e93c93a9df8f8c29c4d847c25655
Robin <gl.deng@gmail.com>
2013-11-06 20:02:58 +0800

    finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Todo::Application.routes.draw do
 
+  resources :microposts
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   get "users/new"
 
   root to: 'static_pages#home'
+
   match '/signup', to: 'users#new',            via: 'get'
+  match '/signin', to: 'sessions#new',         via: 'get'
+  match '/signout',to: 'sessions#destroy',     via: 'delete'
+
   match '/help',   to: 'static_pages#help',    via: 'get'
   match '/about',  to: 'static_pages#about',   via: 'get'
   match '/contact',to: 'static_pages#contact', via: 'get'
 
-  resources :microposts
-
-  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
